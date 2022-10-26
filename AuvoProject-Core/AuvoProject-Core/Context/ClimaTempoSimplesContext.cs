9d9d7a427e6766e563a857a37dbf271a9669648a
Daniel Kadiwel <98348252+DanielKadiwel@users.noreply.github.com>
2022-10-26 16:45:26 -0300

    alterações 01

--- AuvoProject-Core/AuvoProject-Core/Context/ClimaTempoSimplesContext.cs
+++ AuvoProject-Core/AuvoProject-Core/Context/ClimaTempoSimplesContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AuvoProject_Core.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
---
+++ AuvoProject-Core/AuvoProject-Core/Controllers/CidadesController.cs
@@ -0,0 +1,26 @@
+﻿using AuvoProject_Core.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Web;
+namespace AuvoProject_Core.Controllers
+{
+    public class CidadesController : Controller
+    {
+        private ClimaTempoSimplesContext _db = new ClimaTempoSimplesContext();
+        private readonly ILogger<CidadesController> _logger;
+
+        public CidadesController(ILogger<CidadesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+            var cidades = _db.Cidades.Include(x => x.Estado);
+            return View(cidades.ToList());
+        }
+
+
+
+    }
+}
